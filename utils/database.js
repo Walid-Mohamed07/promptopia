ddcd0879eb1180d22c90c083ee4eabbf0359bfe1
Walid Mohamed <walidmohamed234@gmail.com>
2024-10-07 07:03:19 +0300

    First commit 1

--- utils/database.js
+++ utils/database.js
@@ -14,7 +14,7 @@ export const connectToDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
             dbName: process.env.DB_NAME,
-            useNewUrlParser: true,
+            useUnifiedTopology: true,
         })
 
         isConnected = true;
